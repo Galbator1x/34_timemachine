eca98841cf0ca2ba2d403cf951a8dea3ec948dbb
Ivan Filimonov <snake73w@gmail.com>
2017-08-06 20:23:06 +0700

    add Aldrich font source, redesign timer, fix alerts interval

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var TIMEOUT_IN_SECS = 3 * 60
-var NOTIFY_TIMEOUT_IN_MILISECS = 30 * 1000;
-var timerStyle = 'font-family: "Aldrich"; font-size: 60px; color: rgba(0,0,0,0.25);'
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://fonts.googleapis.com/css?family=Aldrich';
+document.head.appendChild(link);
+
+var TIMEOUT_IN_SECS = 3 * 60;
+var NOTIFY_TIMEOUT_IN_SECS = 30;
+var timerStyle = 'font-family: "Aldrich", sans-serif; font-size: 60px; color: #5f5f5f; background-color: rgba(255, 255, 255, 0.62); width: 190px; height: 60px; border: 3px solid rgba(95, 95, 95, 0.65);'
 var TEMPLATE = `<h1 style='${timerStyle}'><span id="timer-minutes">00</span>:<span id="timer-seconds">00</span></h1>`;
 
 // adds HTML tag to current page
 var timerContainer = document.createElement('div');
-timerContainer.setAttribute("style", "z-index:10000; position: fixed; top: 25px; display: inline-block;");
+timerContainer.setAttribute('style', 'z-index:10000; position: fixed; top: 25px; display: inline-block;');
 var bodyTag = document.body;
 bodyTag.insertBefore(timerContainer, bodyTag.firstChild);
 timerContainer.innerHTML = TEMPLATE;
@@ -16,10 +21,11 @@ function getTimestampInSecs(){
 }
 
 function padZero(number){
-    return ("00" + String(number)).slice(-2);
+    return ('00' + String(number)).slice(-2);
 }
 
 var timestampOnStart = getTimestampInSecs();
+var lastAlertConfirmation = TIMEOUT_IN_SECS;
 
 function displayTimer(){
     var currentTimestamp = getTimestampInSecs();
@@ -31,18 +37,13 @@ function displayTimer(){
 
     document.getElementById('timer-minutes').innerHTML = padZero(minutes);
     document.getElementById('timer-seconds').innerHTML = padZero(seconds);
+
+    if (secsGone > TIMEOUT_IN_SECS && secsGone - lastAlertConfirmation >= NOTIFY_TIMEOUT_IN_SECS) {
+        var mins = Math.floor(secsGone / 60);
+        var secs = secsGone - mins * 60;
+        alert(`Ты на сайте уже ${mins} минут, ${secs} секунд. Пора вернуться к работе`);
+        lastAlertConfirmation = getTimestampInSecs() - timestampOnStart;
+    }
 }
+
 setInterval(displayTimer, 300);
-quotes = ['Молодец!', 'Так держать!', 'Я тобой горжусь!', 'Ещё лучше, чем прежде!',
-          'Я знал, что тебе это по силам!', 'Не останавливайся!'];
-function setupNotifications(){
-    window.setInterval(function(){
-        var currentTimestamp = getTimestampInSecs();
-        var secsGone = currentTimestamp - timestampOnStart;
-        var minutes = Math.floor(secsGone / 60);
-        var seconds = secsGone - minutes * 60;
-        var quote = quotes[Math.floor(Math.random()*quotes.length)];
-        alert(`Ты на сайте уже ${minutes} минут, ${seconds} секунд. ${quote}`);
-    }, NOTIFY_TIMEOUT_IN_MILISECS)
-}
-setTimeout(setupNotifications, TIMEOUT_IN_SECS * 1000);
